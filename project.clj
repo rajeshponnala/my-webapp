b6721e4d8783242136528bfdb147df37d085736e
Your Full Name <Your EMail Id>
2015-09-23 03:38:01 +0530

    first commit

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject my-webapp "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler my-webapp.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]
+                        [hiccup "1.0.2"]
+                        [org.clojure/java.jdbc "0.2.3"]
+                        [com.h2database/h2 "1.3.170"]
+                        [ring/ring-jetty-adapter "1.4.0"]]}})
